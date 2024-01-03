de722717e8c02df24c5e813311a04fd51bb4fea7
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2024-01-03 15:07:36 +0900

    add popup

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ import Footer from "components/Layout/Footer/Footer";
 import useData from "context/data";
 
 import HeadMeta from "components/HeadMeta";
+import PopUp from "components/PopUp";
 
 export default function Index3() {
 
@@ -35,6 +36,7 @@ export default function Index3() {
         url="https://xn--h49at0mlnhpml.kr/"
       />
 
+      <PopUp />
 
       <Navbar />
 
