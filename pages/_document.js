dff032599d222cafb9a7f9b983a6e0556a90a8f8
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2025-12-02 13:47:30 +0900

    asdf

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,4 @@
-import Document, { 
-  Html, 
-  Head, 
-  Main, 
-  NextScript 
-} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 class MyDocument extends Document {
@@ -12,22 +7,36 @@ class MyDocument extends Document {
       <Html lang="kr">
         <Head>
           <meta charSet="UTF-8" />
-          <meta name="author" content="동우개발,(주)동우개발,이육행,동우 개발,dongwoo,동우그룹" />
-          <meta name="naver-site-verification" content="b18bbabc4188e134b33cebcb5fc173c123326a2c" />
-          <meta name="google-site-verification" content="n354119AK7RYoyy59Xyvx8hOXBTtuYtF95kw69sVZbU" />
+          <meta
+            name="author"
+            content="동우개발,(주)동우개발,이육행,동우 개발,dongwoo,동우그룹"
+          />
+          <meta
+            name="naver-site-verification"
+            content="b18bbabc4188e134b33cebcb5fc173c123326a2c"
+          />
+          <meta
+            name="google-site-verification"
+            content="n354119AK7RYoyy59Xyvx8hOXBTtuYtF95kw69sVZbU"
+          />
 
-
-          <link rel="shortcut icon" href="https://xn--h49at0mlnhpml.kr/images/favicon.png" />
-          <Script src="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
+          <link
+            rel="shortcut icon"
+            href="https://xn--h49at0mlnhpml.kr/favicon.ico"
+          />
+          <Script
+            src="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
             strategy="beforeInteractive"
           />
-          <Script src="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i&display=swap"
+          <Script
+            src="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i&display=swap"
             strategy="beforeInteractive"
           />
           <Script
             src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=da751fb23c7f8cc96a72163af91ed742&libraries=services,clusterer&autoload=false`}
             strategy="beforeInteractive"
           />
+          <meta property="og:site_name" content="동우개발" />
         </Head>
         <body>
           <Main />
